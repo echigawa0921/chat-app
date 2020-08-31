6bb2adbc75f9e72232650392cb5bb47105c6925d
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-08-31 21:05:33 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
     end
   
     def update
-      if current_user.update(user_params)
-        redirect_to root_path
-      else
-        render :edit
-      end
+        if current_user.update(user_params)
+          redirect_to root_path
+        else
+          render :edit
+        end
     end
   
     private
@@ -16,4 +16,4 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email)
     end
-  end
+end
